4ebf2c1612c55866ae6b21792b077196a7219471
lookrecius <bragin.boris@ya.ru>
2020-04-09 19:30:28 +0300

    Added: express() server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'client')))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
+
+app.listen(3333, function () {
+    console.log("Server has been started on port: 3333")
+})
+
