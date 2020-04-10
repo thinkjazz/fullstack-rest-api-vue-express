26e7c9e69dc171937daa33562277789d361c5992
lookrecius <bragin.boris@ya.ru>
2020-04-10 13:43:26 +0300

    Added: v4 uuid lib + add post method()

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const path = require('path');
+const {v4} = require('uuid');
 const app = express();
 
 const CONTACTS = [
-    {id: 1, name: 'Sudo Rootovitch', value: '+7 958-555-54-21', marked: false}
+    {id: v4(), name: 'Sudo Rootovitch', value: '+7 958-555-54-21', marked: false}
 ]
 
+app.use(express.json())
 // GET
 app.get('/api/contacts', (req, res) => {
     res.status(200).json(CONTACTS)
     console.log(data)
 })
 
+//POST
+
+app.post('/api/contacts', (req, res) => {
+    let contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+})
 
 
 
-
-
-
-app.use(express.static(path.resolve(__dirname, 'client')))
+    app.use(express.static(path.resolve(__dirname, 'client')))
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
 })
